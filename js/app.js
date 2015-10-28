91a2c4cbee9ea2e1c1898e06ca4112e8101166cd
Carl-Philip <carli@launix.de>
2015-10-28 16:06:30 +0100

    Logik Game von ng-app getrennt

---
+++ js/app.js
@@ -0,0 +1,38 @@
+
+
+var app = angular.module('app', []);
+app.controller('main', function ($scope) {
+	$scope.feldtypen = feldtypen;
+	$scope.schildtypen = schildtypen;
+
+	game = new Game(16, 16);
+	game.player = game.players[1];
+
+	setInterval(function () {
+		game.simulate();
+		redrawMap();
+		$scope.$apply();
+	}, 1000);
+
+	$scope.game = game;
+
+	$scope.border = 50;
+
+	var mapctx = $('#map')[0].getContext('2d');
+	function redrawMap() {
+		$scope.feld = game.map.feld[game.player.x][game.player.y];
+		$scope.at = [$scope.feld.at(0), $scope.feld.at(1), $scope.feld.at(2), $scope.feld.at(3)];
+
+		game.map.render(mapctx, game.player);
+	}
+
+	redrawMap();
+
+	$scope.action = function (action) {
+		if (!game.player.doAction(action)) {
+			alert('Aktion nicht erfolgreich');
+		}
+	}
+
+	$scope.neuschild = {};
+});
