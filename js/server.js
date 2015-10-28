ab55d53eaf3d638cfe73e19712db77fa59afbd41
Carl-Philip <carli@launix.de>
2015-10-28 21:53:21 +0100

    Server-API

--- js/server.js
+++ js/server.js
@@ -11,14 +11,22 @@ var Game = require('./game.js');
 var game = new Game(32, 32);
 game.spawnPlayers(1000);
 
-console.log(JSON.stringify(game.export(), true, 2));
-console.log(JSON.stringify(game.map.exportOverview()));
-console.log(JSON.stringify(game.exportPlayer(1), true, 2));
+app.get('/api/game', function (req, res) {
+	res.send(JSON.stringify(game.export(), true, 2));
+});
+
+app.get('/api/map', function (req, res) {
+	res.send(JSON.stringify(game.map.exportOverview()));
+});
+
+app.get('/api/view', function (req, res) {
+	if (req.query.id) {
+		res.send(JSON.stringify(game.exportPlayer(Number(req.query.id))));
+	}
+});
 
 setInterval(function () {
 	game.simulate();
-	var state = JSON.stringify(game.exportPlayer(1), true, 2);
-	console.log(state);
 }, 1000);
 
 app.listen(3000);
