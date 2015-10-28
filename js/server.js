b1c0c54fdb597d4723475d2073ae7287fc920c11
Carl-Philip <carli@launix.de>
2015-10-28 21:47:13 +0100

    HTTP-Server

--- js/server.js
+++ js/server.js
@@ -1,6 +1,15 @@
+// HTTP-Logik
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/..'));
+
+// Spiel-Logik
 var Game = require('./game.js');
 
 var game = new Game(32, 32);
+game.spawnPlayers(1000);
 
 console.log(JSON.stringify(game.export(), true, 2));
 console.log(JSON.stringify(game.map.exportOverview()));
@@ -11,3 +20,5 @@ setInterval(function () {
 	var state = JSON.stringify(game.exportPlayer(1), true, 2);
 	console.log(state);
 }, 1000);
+
+app.listen(3000);
